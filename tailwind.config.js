a39e35a77ee1a53b099c1f37eceeb0b334765b4c
artist0408 <invulnerableartist@gmail.com>
2024-03-31 14:42:45 +0800

    add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,18 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './vendor/wire-elements/modal/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
+    safelist: [
+        'bg-blue-300',
+        'bg-blue-600',
+    ],
     theme: {
         extend: {
             fontFamily: {
